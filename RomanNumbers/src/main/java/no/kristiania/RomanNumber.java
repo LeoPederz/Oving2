51ca605d5c17ed40a3fb33a841139c1084fb75ba
leopedersen <leo-peder@hotmail.com>
2021-08-31 13:21:12 +0200

    finished 3 Roman tasks

--- RomanNumbers/src/main/java/no/kristiania/RomanNumber.java
+++ RomanNumbers/src/main/java/no/kristiania/RomanNumber.java
@@ -1,14 +1,37 @@
 package no.kristiania;
 
+import java.util.Scanner;
+
 public class RomanNumber {
 
     public static void main(String[] args) {
+        System.out.println("Which number do you want?");
+
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        System.out.printf("%d in roman notation is %s", number, toRoman(number));
 
     }
 
-    public String toRoman(int number){
-            return "I";
+    /*
+        public static String toRoman(int number) {
+
+            if(number == 3) {
+                return "III";
+                }
+            if(number == 2) {
+                return "II";
+            }
+            return "II";
         }
 
-
+     */
+    public static String toRoman(int number) {
+        String result = "";
+        for (int i = 0; i < number; i++) {
+            result += "I";
+        }
+            return result;
+    }
 }
+
--- RomanNumbers/src/test/java/no/kristiania/RomanNumberTest.java
+++ RomanNumbers/src/test/java/no/kristiania/RomanNumberTest.java
@@ -5,11 +5,25 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RomanNumberTest {
-    RomanNumber romanNumber = new RomanNumber();
+
 
     @Test
     void shouldConvert1ToI() {
-        assertEquals("I", romanNumber.toRoman(1));
+
+        assertEquals("I", RomanNumber.toRoman(1));
     }
+
+    @Test
+    void shouldConvert2ToII() {
+
+        assertEquals("II", RomanNumber.toRoman(2));
+    }
+
+    @Test
+    void shouldConvert3ToIII() {
+
+        assertEquals("III", RomanNumber.toRoman(3));
+    }
+
 }
 
